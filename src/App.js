26b3934ca4368f32851ce72fb6b707032d5376dd
Ryan MacLennan <ram1229@rit.edu>
2020-09-30 10:00:18 -0400

    added component with states
    added roll dice component that takes the die component and renders 2 of them

--- src/App.js
+++ src/App.js
@@ -2,15 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Die from './die';
+import RollDice from './RollDice'
 
 
 function App() {
   return (
     <div className="App">
-      <Die face="five"/>
-      <Die face="four"/>
-      <Die face="six"/>
-      <Die face="five"/>
+      <RollDice/>
     </div>
   );
 }
